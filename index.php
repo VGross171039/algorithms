4389fe2b39f88f011b7b21dc63c8a66c93a921a5
Vladlen Schetinin <vvschetinin@gmail.com>
2022-05-29 17:08:19 +0300

    еее

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php $root = $_SERVER['DOCUMENT_ROOT']; ?>
 <?php require_once $root . '/template/shared/header.php'; ?>
 
-  <?php if(file_exists($root . '/template/inc/menu/main.php')){
-    require_once $root . '/template/inc/menu/main.php';
-  }?>
+<?php if(file_exists($root . '/template/inc/menu/main.php')){
+  require_once $root . '/template/inc/menu/main.php';
+}?>
 
 <?php require_once $root . '/template/shared/footer.php'; ?>
